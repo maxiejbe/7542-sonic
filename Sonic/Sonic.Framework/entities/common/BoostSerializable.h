4ac030a5f0e1dd340f5a62b0ef09de5cadce8d03
Matias Tebele <matias.tebele@codeait.com>
2017-05-05 20:11:15 -0300

    Fixed serializable inline methods.

--- Sonic/Sonic.Framework/entities/common/BoostSerializable.h
+++ Sonic/Sonic.Framework/entities/common/BoostSerializable.h
@@ -9,32 +9,31 @@
 class BoostSerializable
 {
 public:
-	/*template <class T>
+	template <class T>
 	static void serialize_save(const T& obj, std::string& outString);
 
 	template <class T>
-	static void serialize_load(T& dataOut, const void* data, const size_t dataSize);*/
-
-	template <class T>
-	static void BoostSerializable::serialize_save(const T& obj, std::string& outString)
-	{
-		stringstream binaryOut;
-		boost::archive::binary_oarchive outArchive(binaryOut);
-		outArchive << obj;
-
-		outString = binaryOut.str();
-	}
-
-	template <class T>
-	static void BoostSerializable::serialize_load(T& dataOut, const void* data, const size_t dataSize)
-	{
-		const char* dataPtr = reinterpret_cast<const char*>(data);
-		std::string dataString(dataPtr, dataPtr + dataSize);
-		std::stringstream dataStream(dataString);
-		boost::archive::binary_iarchive binArchive(dataStream);
-		binArchive >> dataOut;
-	}
+	static void serialize_load(T& dataOut, const void* data, const size_t dataSize);
 };
 
 #endif // !BOOST_SERIALIZABLE_H
 
+template <class T>
+inline void BoostSerializable::serialize_save(const T& obj, std::string& outString)
+{
+	stringstream binaryOut;
+	boost::archive::binary_oarchive outArchive(binaryOut);
+	outArchive << obj;
+
+	outString = binaryOut.str();
+}
+
+template <class T>
+inline void BoostSerializable::serialize_load(T& dataOut, const void* data, const size_t dataSize)
+{
+	const char* dataPtr = reinterpret_cast<const char*>(data);
+	std::string dataString(dataPtr, dataPtr + dataSize);
+	std::stringstream dataStream(dataString);
+	boost::archive::binary_iarchive binArchive(dataStream);
+	binArchive >> dataOut;
+}
