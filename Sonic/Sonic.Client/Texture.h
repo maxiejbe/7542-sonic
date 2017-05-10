ac0a45d13c4d1843e4a46d7e894c74a88ea74ac3
Matias Tebele <matias.tebele@codeait.com>
2017-05-09 23:38:22 -0300

    Freeing resources on Client.

--- Sonic/Sonic.Client/Texture.h
+++ Sonic/Sonic.Client/Texture.h
@@ -13,16 +13,12 @@ public:
 
 	bool loadFromFile(std::string path);
 	void free();
-	//void setColor(Uint8 red, Uint8 green, Uint8 blue);
-	//void setBlendMode(SDL_BlendMode blending);
-	//void setAlpha(Uint8 alpha);
 	void render(int x, int y, SDL_Rect* clip = NULL);
-	//void render(int x, int y, SDL_Rect* clip, double angle, SDL_Point* center, SDL_RendererFlip flip);
-	void Texture::render(int x, int y, SDL_Rect* clip, SDL_Rect dest, double angle, SDL_Point* center, SDL_RendererFlip flip);
+	void render(int x, int y, SDL_Rect* clip, SDL_Rect dest, double angle, SDL_Point* center, SDL_RendererFlip flip);
 
 	int getWidth();
 	int getHeight();
-	SDL_Texture* getTexture(); // es necesario?
+	SDL_Texture* getTexture();
 
 protected:
 	SDL_Texture* texture;
