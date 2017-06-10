4ce5390bec60a7981d6dd2462ef77bedc171efbd
Matias Tebele <matias.tebele@codeait.com>
2017-06-10 17:41:28 -0300

    Fixed several memory leaks.

--- Sonic/Sonic.Framework/protocol/SerializableMessage.cpp
+++ Sonic/Sonic.Framework/protocol/SerializableMessage.cpp
@@ -10,9 +10,11 @@ bool SerializableMessage::fromJson(string json)
 	const char* message = StringUtils::convert(json);
 	if (document.Parse(message).HasParseError()) {
 		LOG(logERROR) << "No se pudo deserializar el mensaje: " << json;
+		delete message;
 		return false;
 	}
 
+	delete message;
 	unserialize(&document);
 	return true;
 }
