60c4b91cea6e4b65c08779f5cff9bbe97da649c4
Federico Di Rocco <fede.dirocco@hotmail.com>
2017-06-13 19:42:27 -0300

    Add game over state

---
+++ Sonic/Sonic.Client/GameOverState.cpp
@@ -0,0 +1,39 @@
+#include "GameOverState.h"
+#include "Game.h"
+
+GameOverState GameOverState::m_GameOverState;
+
+
+void GameOverState::load(Game* game)
+{
+	gameOverBackground.loadFromFile("img/gameover.jpg");
+
+	this->showGameOver();
+}
+
+void GameOverState::update(Game* game, float dt)
+{
+	InputManager* input = InputManager::getInstance();
+	input->update();
+
+	if (input->isKeyDown(KEY_RETURN)) {
+		// Disconnect
+		NetworkManager::getInstance().disconnect();
+		// Go to menu
+		game->changeState(MenuState::Instance());
+	}
+}
+
+void GameOverState::render(Game* game)
+{
+}
+
+int GameOverState::unload()
+{
+	return 0;
+}
+
+void GameOverState::showGameOver()
+{
+	gameOverBackground.render(0, 0, gameOverBackground.getWidth(), gameOverBackground.getHeight());
+}
